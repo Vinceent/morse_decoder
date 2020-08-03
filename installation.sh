8184dc7eb26c0da66229f666b41607f2fa118a8f
Vinceent <artemkytkin@gmail.com>
2020-08-03 12:46:00 +0300

    description fix

--- installation.sh
+++ installation.sh
@@ -10,6 +10,6 @@ rm temp_build.pro
 rm Makefile
 
 python gen_test_code.py
-echo "\n\nInstalation's over. 2 executables, namely \"morse_decoder\" and \"graphic_visualization\" generated and copied from the coresponding folders. Aslo 2 additional files were created. File \"test_code\" provides a testing example for the program. File \"morse"\ is just a morse table for reference."
+echo "\n\nInstalation's over. 2 executables, namely \"morse_decoder\" and \"graphic_visualization\" generated and copied from the coresponding folders. Aslo 3 additional files were created. File \"test_code\" provides a testing example for the program.File \"gen_test_code.py\" allows you to automaticly generate new \"test_code\" files. File \"morse\" is just a morse table for reference."
 
 
